08b1a399130ed65d0c12c3e4b02707e72b261b70
Jacob Devera <jacob.devera12@gmail.com>
2017-01-23 22:40:01 -0800

    Created project and initial layout

---
+++ app/src/main/java/edu/washington/gjdevera/lifecounter/MainActivity.java
@@ -0,0 +1,17 @@
+package edu.washington.gjdevera.lifecounter;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
