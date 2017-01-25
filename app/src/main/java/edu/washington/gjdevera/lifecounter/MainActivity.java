8314d708c3b5c4000e2df0998606f04fbba1e71a
Jacob Devera <jacob.devera12@gmail.com>
2017-01-25 00:14:51 -0800

    Added ability to add players (up to eight)

--- app/src/main/java/edu/washington/gjdevera/lifecounter/MainActivity.java
+++ app/src/main/java/edu/washington/gjdevera/lifecounter/MainActivity.java
@@ -1,22 +1,21 @@
 package edu.washington.gjdevera.lifecounter;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.ArrayList;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
-
-    private int players = 4;
-    private List<Integer> scores = new ArrayList<>(Arrays.asList(0,0,0,0));
+    
+    // start with four players with twenty lives
+    private List<Integer> lives = new ArrayList<>();
     private LinearLayout ll;
 
     @Override
@@ -25,21 +24,17 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         ll = (LinearLayout) findViewById(R.id.activity_main);
 
-        // create buttons for all new players
-        for (int i = 0; i < players; i++) {
-            TextView tv = new TextView(this);
-            tv.setText("Player " + (i + 1) + ": " + scores.get(i));
-            tv.setId(i);
-            ll.addView(tv);
+        final Button btnAdd = (Button) findViewById(R.id.btn_add);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addPlayer();
+            }
+        });
 
-            LinearLayout buttonLayout = new LinearLayout(this);
-            buttonLayout.setOrientation(LinearLayout.HORIZONTAL);
-            buttonLayout.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT,LayoutParams.WRAP_CONTENT, 1f));
-            initButton(new Button(this), "+", i, 1, buttonLayout);
-            initButton(new Button(this), "-", i, -1, buttonLayout);
-            initButton(new Button(this), "+5", i, 5, buttonLayout);
-            initButton(new Button(this), "-5", i, -5, buttonLayout);
-            ll.addView(buttonLayout);
+        // create buttons for all new players
+        for (int i = 0; i < 4; i++) {
+            addPlayer();
         }
     }
 
@@ -51,26 +46,27 @@ public class MainActivity extends Activity {
         ll.addView(button);
     }
 
-    /*public void addPlayer(LinearLayout ll) {
+    public void addPlayer() {
+        int players = lives.size();
         if (players > 7) {
-            // too many players
-        } else if (players < 3) {
-            // too few players
-        } else  {
-            players++;
+            // already have 8 players
+        } else {
+            lives.add(20);
             TextView tv = new TextView(this);
-            tv.setText("Player " + players);
+            tv.setText("Player " + (players + 1) + ": " + lives.get(players));
+            tv.setId(players);
             ll.addView(tv);
 
-            Button b = new Button(this);
-            b.setText("+");
-            b.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
-            b.setId(players);
-            b.setOnClickListener(new ButtonListener(players));
-            ll.addView(b);
+            LinearLayout buttonLayout = new LinearLayout(this);
+            buttonLayout.setOrientation(LinearLayout.HORIZONTAL);
+            buttonLayout.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT,LayoutParams.WRAP_CONTENT, 1f));
+            initButton(new Button(this), "+", players, 1, buttonLayout);
+            initButton(new Button(this), "-", players, -1, buttonLayout);
+            initButton(new Button(this), "+5", players, 5, buttonLayout);
+            initButton(new Button(this), "-5", players, -5, buttonLayout);
+            ll.addView(buttonLayout);
         }
-
-    }*/
+    }
 
     // custom listener for buttons
     public class ButtonListener implements View.OnClickListener {
@@ -84,9 +80,9 @@ public class MainActivity extends Activity {
 
         @Override
         public void onClick(View v) {
-            scores.set(player, scores.get(player) + modifier);
+            lives.set(player, lives.get(player) + modifier);
             TextView tv = (TextView) findViewById(player);
-            tv.setText("Player " + (player + 1) + ": " + scores.get(player));
+            tv.setText("Player " + (player + 1) + ": " + lives.get(player));
         }
     }
 }
